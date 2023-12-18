8c2ada79bac4cf65f2db52075bf39bbe1929114b
Nomio <nomikeeper@gmail.com>
2023-12-18 16:03:18 +0900

    Premake changes.

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+  cppdialect "c++17"
+  staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -23,8 +25,14 @@ project "ImGui"
 
   filter "system:windows"
     systemversion "latest"
-    cppdialect "c++17"
-    staticruntime "On"
+
+  filter "configurations:Debug"
+      runtime "Debug"
+      symbols "on"
+      
+  filter "configurations:Release"
+      runtime "Release"
+      optimize "on"
 
   filter { "system:windows", "configurations:Release" }	
     buildoptions "/MT"
